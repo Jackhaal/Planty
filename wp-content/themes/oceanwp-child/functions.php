e1fa42f3769c6cefb62fe175ca37b5ff773815f4
Jackhaal <56018134+Jackhaal@users.noreply.github.com>
2024-05-15 11:10:13 +0200

    derniers réglages

--- wp-content/themes/oceanwp-child/functions.php
+++ wp-content/themes/oceanwp-child/functions.php
@@ -25,14 +25,21 @@ add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
 add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
 
-function add_extra_item_to_nav_menu( $items, $args ) {
 
-    var_dump("test");
-    if (is_user_logged_in() /*&& $args->menu == 2*/) {
+function add_extra_item_to_nav_menu( $items, $args ) {
+    /*
+    echo("<br>");
+    echo("<br>");
+    echo("<br>");
+    echo("<br>");
+    echo("<br>");
+    
+    echo('<pre style="position: absolute;top:0;left:0">');
+    var_dump($args->menu);
+    echo("</pre>");  */ 
+    if (is_user_logged_in() && $args->menu_id == "menu-1-4894065") {
         $items .= '<li class="menu-item menu-item-type-post_type menu-item-object-page parent hfe-creative-menu"><a href="/wp-Admin" class="hfe-menu-item">Admin</a></li>';
     }
-    elseif (!is_user_logged_in() /*&& $args->menu == 2*/) {
-        $items .= '<li class="menu-item menu-item-type-post_type menu-item-object-page parent hfe-creative-menu"><a href="/wp-Admin" class="hfe-menu-item">Se connecter</a></li>';
-    }
+   
     return $items;
 }
